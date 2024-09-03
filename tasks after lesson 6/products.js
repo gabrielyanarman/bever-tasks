5500b1308fd1ce2a868806396080b26b4f935e68
gabrielyanarman <armangabrielyan08@gmail.com>
2024-09-03 16:35:30 +0400

    optimize code in product js

--- tasks after lesson 6/products.js
+++ tasks after lesson 6/products.js
@@ -1,7 +1,7 @@
 function checkType(executionContext) {
     const formContext = executionContext.getFormContext();
-    const productType = formContext.getAttribute("new_os_type").getText();
-    productType === "Product"
-      ? formContext.getControl("new_mon_price_per_unit").setVisible(true)
-      : formContext.getControl("new_mon_price_per_unit").setVisible(false);
+    const productTypeValue = formContext.getAttribute("new_os_type").getValue();
+    productTypeValue === 100000000
+        ? formContext.getControl("new_mon_price_per_unit").setVisible(true)
+        : formContext.getControl("new_mon_price_per_unit").setVisible(false);
 }
