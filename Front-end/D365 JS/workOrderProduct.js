8f9cd0458e2fe6026b5cf26f3affaf302d8e89a6
gabrielyanarman <armangabrielyan08@gmail.com>
2024-09-11 10:42:18 +0400

    change addCustomView to addCustomFilter

--- Front-end/D365 JS/workOrderProduct.js
+++ Front-end/D365 JS/workOrderProduct.js
@@ -1,9 +1,49 @@
-function onChangeInventory(executionContext) {
-    const formContext = executionContext.getFormContext();
-    formContext.getControl("new_fk_product").addPreSearch(createProductsCustomView);
+let productLookupPointer = null;
+
+async function onChangeInventory(executionContext) {
+  const formContext = executionContext.getFormContext();
+  const inventoryLookup = formContext.getAttribute("new_fk_inventory").getValue();
+
+  if(productLookupPointer !== null) formContext.getControl("new_fk_product").removePreSearch(productLookupPointer);
+  if(inventoryLookup === null || !inventoryLookup.length) return;
+
+  const inventoryId = inventoryLookup[0].id;
+
+  let fetchXml = `
+    <fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="true">
+      <entity name="new_product">
+      <attribute name="new_productid"/>
+      <attribute name="new_name"/>
+      <attribute name="createdon"/>
+      <order attribute="new_name" descending="false"/>
+      <filter type="and">
+        <condition attribute="new_os_type" operator="eq" value="100000000"/>
+      </filter>
+        <link-entity name="new_inventory_product" from="new_fk_product" to="new_productid" link-type="inner" alias="ip">
+          <filter type="and">
+            <condition attribute="new_fk_inventory" operator="eq" value="${inventoryId}"/>
+          </filter>
+        </link-entity>
+      </entity>
+    </fetch>
+  `;
+
+  fetchXml = "?fetchXml=" + encodeURIComponent(fetchXml);
+
+  try {
+    const fetchResult = await Xrm.WebApi.retrieveMultipleRecords("new_product", fetchXml);
+    const filteredProducts = fetchResult.entities;
+
+    productLookupPointer = filterProducts.bind({ filteredProducts });
+
+    formContext.getControl("new_fk_product").addPreSearch(productLookupPointer);
+  } catch (error) {
+    console.error(error)
+  } 
 }
 
-function createProductsCustomView(executionContext) {
+function filterProducts(executionContext) {
+
   const formContext = executionContext.getFormContext();
   const control = formContext.getControl("new_fk_product");
   if (!control) {
@@ -11,68 +51,14 @@ function createProductsCustomView(executionContext) {
     return;
   }
 
-  const inventoryLookup = formContext.getAttribute("new_fk_inventory").getValue();
-  let isInventorySelected = true;
-  let inventoryId = null;
+  let filter = `<filter type="or">`
 
-  if (inventoryLookup === null || !inventoryLookup.length) {
-    isInventorySelected = false;
-  } else {
-    inventoryId = inventoryLookup[0].id;
-  }
 
-  const fetchXmlProducts = `
-    <fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="false">
-        <entity name="new_product">
-            <attribute name="new_productid"/>
-            <attribute name="new_name"/>
-            <attribute name="createdon"/>
-            <order attribute="new_name" descending="false"/>
-            <filter type="and">
-                <condition attribute="new_os_type" operator="eq" value="100000000"/>
-            </filter>
-        </entity>
-    </fetch>
-  `;
+  this.filteredProducts.forEach((product) => {
+    filter += `<condition attribute="new_productid" operator="eq" value="${product["new_productid"]}"/>`;
+  })
 
-  const fetchXmlFilteredProducts = `
-    <fetch version="1.0" output-format="xml-platform" mapping="logical" distinct="true">
-        <entity name="new_product">
-        <attribute name="new_productid"/>
-        <attribute name="new_name"/>
-        <attribute name="createdon"/>
-        <order attribute="new_name" descending="false"/>
-        <filter type="and">
-            <condition attribute="new_os_type" operator="eq" value="100000000"/>
-        </filter>
-        <link-entity name="new_inventory_product" from="new_fk_product" to="new_productid" link-type="inner" alias="ab">
-            <filter type="and">
-                <condition attribute="new_fk_inventory" operator="eq" value="${inventoryId}"/>
-            </filter>
-        </link-entity>
-        </entity>
-    </fetch>
-  `;
+  filter += `</filter>`;
 
-  const fetchXml = isInventorySelected ? fetchXmlFilteredProducts : fetchXmlProducts;
-
-  const viewId = "00000000-0000-0000-0000-000000000111";
-  const viewDisplayName = "Filtered View";
-  const layoutXml = `
-    <grid name="resultset" object="1" jump="id" select="1" icon="1" preview="1">
-    <row name="result" id="new_productid">
-        <cell name="new_name" width="150" />
-        <cell name="createdon" width="150" />
-    </row>
-    </grid>
-  `;
-
-  control.addCustomView(
-    viewId,
-    "new_product",
-    viewDisplayName,
-    fetchXml,
-    layoutXml,
-    true
-  );
+  control.addCustomFilter(filter, "new_product")
 }
